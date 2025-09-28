1d8a8a6535dc00c31ff82ff0e856798a481221f7
Thimote147 <thimote.fetu@student.vinci.be>
2025-09-28 14:37:20 +0200

    feat: enhance CORS configuration for improved security and flexibility

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,9 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ['http://localhost:5173', /\.onrender\.com$/],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
   })
 );
 
